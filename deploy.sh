4ff38e06edd3c9ade2d12711b496199585b40354
Dan Cascaval <dan@cascaval.us>
2024-05-05 23:16:24 -0700

    replace

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-# Copies the generated JS to dist.
-# Before this script runs:
-# - Index HTML that includes this file should be generated
-#    (`sbt portfolioJVM/run (with prodction = true)`)
-# - Optimized javascript should be generated.
-#   (`sbt fullOptJS`)
-cp js/target/scala-3.0.2/portfolio-opt.js dist/interaction.js
